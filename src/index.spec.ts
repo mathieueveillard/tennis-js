17b731371e83d0a9addf8121ffdbdf8c47046b5b
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2020-06-06 06:43:46 +0200

    Version 1

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,67 @@
-import { doTheThing } from ".";
+import { Game } from ".";
 
-describe("Test of the test framework", function() {
-  it("Should pass", function() {
-    expect(doTheThing()).toEqual(true);
-  });
+function toDeuceState() {
+  return new Game()
+    .score("PLAYER1")
+    .score("PLAYER2")
+    .score("PLAYER1")
+    .score("PLAYER2")
+    .score("PLAYER1")
+    .score("PLAYER2");
+}
+
+it("Score should be blank on game initialisation", function() {
+  const game = new Game();
+  expect(game.displayScore()).toEqual("Love-Love");
+});
+
+it("1 point should be displayed as '15'", function() {
+  const game = new Game().score("PLAYER1");
+  expect(game.displayScore()).toEqual("15-Love");
+});
+
+it("2 points should be displayed as '30'", function() {
+  const game = new Game().score("PLAYER1").score("PLAYER1");
+  expect(game.displayScore()).toEqual("30-Love");
+});
+
+it("3 points should be displayed as '40'", function() {
+  const game = new Game()
+    .score("PLAYER1")
+    .score("PLAYER1")
+    .score("PLAYER1");
+  expect(game.displayScore()).toEqual("40-Love");
+});
+
+it("If a player has scored 40 and they score next, they win the game", function() {
+  const game = new Game()
+    .score("PLAYER1")
+    .score("PLAYER1")
+    .score("PLAYER1")
+    .score("PLAYER1");
+  expect(game.displayScore()).toEqual("PLAYER1 wins");
+});
+
+it("If both players have scored 40, the game is said to be 'deuce'", function() {
+  const game = toDeuceState();
+  expect(game.displayScore()).toEqual("Deuce");
+});
+
+it("If the game is deuce, the next scoring player has 'advantage'", function() {
+  const game = toDeuceState().score("PLAYER1");
+  expect(game.displayScore()).toEqual("Advantage PLAYER1");
+});
+
+it("If the player with advantage scores next, they win the game", function() {
+  const game = toDeuceState()
+    .score("PLAYER1")
+    .score("PLAYER1");
+  expect(game.displayScore()).toEqual("PLAYER1 wins");
+});
+
+it("If the player without advantage scores next, the game is back to deuce", function() {
+  const game = toDeuceState()
+    .score("PLAYER1")
+    .score("PLAYER2");
+  expect(game.displayScore()).toEqual("Deuce");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,81 @@
-export function doTheThing(): boolean {
-  return true;
+type Player = "PLAYER1" | "PLAYER2";
+
+function other(player: Player): Player {
+  return player === "PLAYER1" ? "PLAYER2" : "PLAYER1";
+}
+
+type Point = 0 | 1 | 2 | 3;
+
+type Points = Record<Player, Point>;
+
+const PointsOnStartingGame: Points = {
+  PLAYER1: 0,
+  PLAYER2: 0
+};
+
+const PointsDisplay: Record<Point, string> = {
+  0: "Love",
+  1: "15",
+  2: "30",
+  3: "40"
+};
+
+interface IGame {
+  score(player: Player): IGame;
+  displayScore(): string;
+}
+
+export class Game implements IGame {
+  constructor(private points: Points = PointsOnStartingGame) {}
+
+  score(player: Player) {
+    if (this.points[player] === 3) {
+      return new EndedGame(player);
+    }
+    if (this.points[player] === 2 && this.points[other(player)] === 3) {
+      return new DeuceGame();
+    }
+    return new Game({
+      ...this.points,
+      [player]: this.points[player] + 1
+    });
+  }
+
+  displayScore() {
+    return `${PointsDisplay[this.points.PLAYER1]}-${PointsDisplay[this.points.PLAYER2]}`;
+  }
+}
+
+class EndedGame implements IGame {
+  constructor(private winner: Player) {}
+
+  score(): IGame {
+    throw Error("This game has already ended!");
+  }
+
+  displayScore() {
+    return `${this.winner} wins`;
+  }
+}
+
+class DeuceGame implements IGame {
+  score(player: Player) {
+    return new AdvantageGame(player);
+  }
+
+  displayScore() {
+    return `Deuce`;
+  }
+}
+
+class AdvantageGame implements IGame {
+  constructor(private advantagedPlayer: Player) {}
+
+  score(player: Player) {
+    return player === this.advantagedPlayer ? new EndedGame(player) : new DeuceGame();
+  }
+
+  displayScore() {
+    return `Advantage ${this.advantagedPlayer}`;
+  }
 }
